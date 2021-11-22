204f6371eef8609d5b15054c929782b0a13deee4
Carl Contreras <contrerascarl09@gmail.com>
2021-11-21 18:16:06 -0600

    added var and const in js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # password-maker
+    Password-maker is a assignment for coding bootcamp. 
+    password-maker code was providing by the bootcamp but are missing 
+    key elements on javascript. 
+    
+## HTML
+    added folders on password-maker
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,34 @@
 // Assignment Code
+var length;
+var number; 
+var upperCase;
+var lowerCase;
+var specialCharacter;
+var selection;
+
+const characters = ["number", "letters", "character"];
+
+number = "0123456789";
+letters = "abcdefghijklmnopqrstuvwxyz";
+lettersUp = "ABCDEFG"
+
+// Get reference to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
  
   passwordText.value = password;
-sw
-  for (var i = 0; i <= passwordLength; i++){
-    var randomNumber = Math.floor(Math.random() * passwordText.length);
-    password += passwordText.substring(randomNumber, randomNumber +1);
-  }
+  
+
 
  
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword, passwordText)
-
+generateBtn.addEventListener("click", writePassword)
