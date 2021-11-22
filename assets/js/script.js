bab85dd84ba41f4c0caf4b4e105ad980ffe162b7
Carl Contreras <contrerascarl09@gmail.com>
2021-11-21 18:42:04 -0600

    js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,8 @@ const characters = ["number", "letters", "character"];
 
 number = "0123456789";
 letters = "abcdefghijklmnopqrstuvwxyz";
-lettersUp = "ABCDEFG"
+lettersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+character = "!@#$%^&*?=+~";
 
 // Get reference to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -21,13 +22,25 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+   passwordText.value = password;
   
- 
-  passwordText.value = password;
+}
+
+function generatePassword() {
+  length = kprompt("Enter the number of characters you'd like for your new password (Select a number between 8-128)");
+  if (!length) {
+    alert("Please make a valid entry");
+  }
+  else if (length < 8 || length > 128) {
+    length = prompt("New password must be from 8-128 characters");
+  }
+  else {
+    number = confirm("Include numbers in your new password?");
+    specialCharacter = confirm("Include uppercase letters?");
+    upperCase = confirm("Include uppercase letters?");
+    lowerCase = confrim("Include lowercase letters?");
+  };
   
-
-
- 
 }
 
 // Add event listener to generate button
